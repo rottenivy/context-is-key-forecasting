a7f64050c4f391f0eb7104c04b64b6d9a37d6ec8
Étienne Marcotte <etienne.marcotte@servicenow.com>
2024-07-19 17:12:32 +0000

    Update the example baselines for multivariate data format

--- benchmark/example_eval.py
+++ benchmark/example_eval.py
@@ -15,8 +15,12 @@ def random_baseline(task_instance, n_samples=50):
     A baseline is just some callable that receives a task instance and returns a prediction.
 
     """
-    # This is only valid for a univariate forecast, of shape [samples, time dimension, 1]
-    return np.random.rand(n_samples, task_instance.future_time.shape[0], 1)
+    # This is valid for forecasts of shape [samples, time dimension, number of variables]
+    return np.random.rand(
+        n_samples,
+        task_instance.future_time.shape[0],
+        task_instance.future_time.shape[1],
+    )
 
 
 def oracle_baseline(task_instance, n_samples=50):
@@ -24,10 +28,13 @@ def oracle_baseline(task_instance, n_samples=50):
     A perfect baseline that looks at the future and returns it in multiple copies with a tiny jitter (like perfect samples)
 
     """
-    # This is only valid for a univariate forecast, of shape [samples, time dimension, 1]
+    # This is valid for forecasts of shape [samples, time dimension, number of variables]
+    target = (
+        task_instance.future_time.to_numpy()
+    )  # [time dimension, number of variables]
     return (
-        np.tile(task_instance.future_time.values[None, :, None], (n_samples, 1, 1))
-        + np.random.rand(n_samples, task_instance.future_time.shape[0], 1) * 1e-6
+        target[None, :, :]
+        + np.random.rand(n_samples, target.shape[0], target.shape[1]) * 1e-6
     )
 
 
