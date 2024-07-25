ab087724b3ea55d2186686dd93d6215de6324224
Alexandre Drouin <alexandre.drouin1@gmail.com>
2024-07-25 11:04:59 +0200

    Add GPT Processes

--- benchmark/benchmark/baselines/utils.py
+++ benchmark/benchmark/baselines/utils.py
@@ -1,4 +1,35 @@
+import re
 import torch
+
+
+def extract_html_tags(text, keys):
+    """Extract the content within HTML tags for a list of keys.
+
+    Parameters
+    ----------
+    text : str
+        The input string containing the HTML tags.
+    keys : list of str
+        The HTML tags to extract the content from.
+
+    Returns
+    -------
+    dict
+        A dictionary mapping each key to a list of subset in `text` that match the key.
+
+    Notes
+    -----
+    All text and keys will be converted to lowercase before matching.
+
+    """
+    content_dict = {}
+    keys = set(keys)
+    for key in keys:
+        pattern = f"<{key}>(.*?)</{key}>"
+        matches = re.findall(pattern, text, re.DOTALL)
+        if matches:
+            content_dict[key] = [match.strip() for match in matches]
+    return content_dict
 
 
 def torch_default_device():
