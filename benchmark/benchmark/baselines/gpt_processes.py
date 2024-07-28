1a936334dcf89be066d6f4f1ee999efeec16a6bc
Alexandre Drouin <alexandre.drouin1@gmail.com>
2024-07-28 11:02:08 +0200

    Increase robustness via rejection sampling

--- benchmark/benchmark/baselines/gpt_processes.py
+++ benchmark/benchmark/baselines/gpt_processes.py
@@ -107,6 +107,7 @@ Here is a historical time series in (timestamp, value) format:
 Now please predict the value at the following timestamps: {pred_time}.
 
 Return the forecast in (timestamp, value) format in between <forecast> and </forecast> tags.
+Do not include any other information (e.g., comments) in the forecast.
 
 Example:
 <history>
@@ -122,58 +123,99 @@ Example:
 """
         return prompt
 
-    def __call__(self, task_instance, n_samples):
+    def __call__(self, task_instance, n_samples, n_retries=3, batch_size_on_retry=5):
+        """
+        Infer forecasts from the GPT model
+
+        Parameters:
+        -----------
+        task_instance: TimeSeriesTask
+            The task instance to forecast
+        n_samples: int
+            The number of samples to generate
+        n_retries: int
+            The number of rejection sampling steps
+        batch_size_on_retry: int
+            The batch size to use on retries. This is useful to avoid asking for way too many samples
+            from the openai API.
+
+        Returns:
+        --------
+        samples: np.ndarray, shape [n_samples, time dimension, number of variables]
+            The forecast samples. Note: only univariate is supported at the moment (number of variables = 1)
+
+        """
         assert (
             task_instance.past_time.shape[-1] == 1
-        ), "Only uni-variate time series are supported at the moment."
+        ), "Only univariate time series are supported at the moment."
 
         prompt = self.make_prompt(task_instance)
+        messages = [
+            {
+                "role": "system",
+                "content": "You are a useful forecasting assistant.",
+            },
+            {"role": "user", "content": prompt},
+        ]
 
-        logger.info(f"Requesting forecast of {n_samples} samples from GPT model.")
-        chat_completion = self.client.chat.completions.create(
-            model=self.model,
-            n=n_samples,
-            messages=[
-                {
-                    "role": "system",
-                    "content": "You are a useful forecasting assistant.",
-                },
-                {"role": "user", "content": prompt},
-            ],
-        )
-
-        logger.info("Parsing forecasts from completion.")
+        # Get forecast samples via rejection sampling until we have the desired number of samples
+        # or until we run out of retries
+        batch_size = n_samples
         valid_forecasts = []
-        for choice in chat_completion.choices:
-            try:
-                # Extract forecast from completion
-                forecast = extract_html_tags(choice.message.content, ["forecast"])[
-                    "forecast"
-                ][0]
-                forecast = forecast.replace("(", "").replace(")", "")
-                forecast = forecast.split("\n")
-                forecast = {x.split(",")[0]: float(x.split(",")[1]) for x in forecast}
+        while len(valid_forecasts) < n_samples and n_retries > 0:
+            logger.info(f"Requesting forecast of {batch_size} samples from GPT model.")
+            chat_completion = self.client.chat.completions.create(
+                model=self.model, n=batch_size, messages=messages
+            )
 
-                # Get forecasted values at expected timestamps (will fail if model hallucinated timestamps, which is ok)
-                forecast = [
-                    forecast[t]
-                    for t in task_instance.future_time.index.strftime(
-                        "%Y-%m-%d %H:%M:%S"
-                    )
-                ]
-                valid_forecasts.append(forecast)
-            except Exception as e:
-                if self.fail_on_invalid:
-                    raise e
-                logger.info(
-                    "Error: No forecast or incorrect format. Skipping this sample."
-                )
-                logger.debug(f"Exception details: {e}")
-                logger.debug(f"Forecast: {forecast}")
-                logger.debug(f"Choice: {choice.message.content}")
+            logger.info("Parsing forecasts from completion.")
+            for choice in chat_completion.choices:
+                try:
+                    # Extract forecast from completion
+                    forecast = extract_html_tags(choice.message.content, ["forecast"])[
+                        "forecast"
+                    ][0]
+                    forecast = forecast.replace("(", "").replace(")", "")
+                    forecast = forecast.split("\n")
+                    forecast = {
+                        x.split(",")[0]
+                        .replace("'", "")
+                        .replace('"', ""): float(x.split(",")[1])
+                        for x in forecast
+                    }
+
+                    # Get forecasted values at expected timestamps (will fail if model hallucinated timestamps, which is ok)
+                    forecast = [
+                        forecast[t]
+                        for t in task_instance.future_time.index.strftime(
+                            "%Y-%m-%d %H:%M:%S"
+                        )
+                    ]
+
+                    # Append forecast to list of valid forecasts
+                    valid_forecasts.append(forecast)
+
+                except Exception as e:
+                    logger.info("Sample rejected due to invalid format.")
+                    logger.debug(f"Rejection details: {e}")
+                    logger.debug(f"Forecast: {forecast}")
+                    logger.debug(f"Choice: {choice.message.content}")
+
+            n_retries -= 1
+            batch_size = batch_size_on_retry
+
+            valid_forecasts = valid_forecasts[:n_samples]
+            logger.info(f"Got {len(valid_forecasts)}/{n_samples} valid forecasts.")
+            if len(valid_forecasts) < n_samples:
+                logger.info(f"Remaining retries: {n_retries}.")
+
+        # If we couldn't collect enough forecasts, raise exception if desired
+        if self.fail_on_invalid and len(valid_forecasts) < n_samples:
+            raise RuntimeError(
+                f"Failed to get {n_samples} valid forecasts. Got {len(valid_forecasts)} instead."
+            )
 
         # Convert the list of valid forecasts to a numpy array
-        logger.info(f"Got {len(valid_forecasts)} valid forecasts.")
         samples = np.array(valid_forecasts)[:, :, None]
 
         return samples
