25d6c08435b4cbeb36f7f63697796b8c190a6670
Alexandre Drouin <alexandre.drouin1@gmail.com>
2024-07-31 08:59:09 -0400

    Remove dominick for now

--- benchmark/tests/bootstrap_datasets.py
+++ benchmark/tests/bootstrap_datasets.py
@@ -1,5 +1,10 @@
 from benchmark import ALL_TASKS
 
+# TODO: Remove this when dominick data loader is merged
+from benchmark.predictable_grocer_shocks import __TASKS__ as DOMINICK_TASKS
+
+ALL_TASKS = list(set(ALL_TASKS) - set(DOMINICK_TASKS))
+
 print("Downloading datasets for all tasks...")
 for task in ALL_TASKS:
     # Create a random instance of the task
