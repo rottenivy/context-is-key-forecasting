1a944ab46625b47b9da65c901df60444b985f7b3
AndrewRWilliams <williams.andrew1305@gmail.com>
2024-08-07 15:38:51 +0000

    add docstrings, make metric switchable, make sample iteration in constraint checking explicit, test multiple samples

--- benchmark/benchmark/metrics/roi_metric.py
+++ benchmark/benchmark/metrics/roi_metric.py
@@ -12,24 +12,39 @@ def region_of_interest_constraint_metric(
     target,
     forecast,
     region_of_interest,
-    weight,
+    roi_weight,
+    roi_metric=crps_by_quantile_mean,
     constraints=None,
     tolerance_percentage=0.05,
-    roi_metric=crps_by_quantile_mean,
 ):
     """
     Function to calculate a combined metric that considers a region of interest and constraints.
-    Paramters:
+
+    Parameters:
     ----------
     target: np.ndarray
         The target values. (n_timesteps,)
     forecast: np.ndarray
         The forecast values. (n_samples, n_timesteps)
     region_of_interest: int, list of ints, slice, or boolean mask
+        The region of interest to apply the roi_metric to.
+    roi_weight: float
+        The weight to apply to the region of interest.
+    roi_metric: function, optional
+        The metric to apply to the region of interest. Default is crps_by_quantile_mean.
+    constraints: dict, optional
+        A dictionary containing the constraints. The keys are "min" and "max". Default is None.
+    tolerance_percentage: float, optional
+        The percentage of the target range to use as a tolerance for the constraints. Default is 0.05.
+
+    Returns:
+    --------
+    combined_roi_metric_with_penalty: float
+        The combined metric value with the constraint penalty applied.
     """
 
     combined_roi_metric = calculate_combined_roi_metric(
-        target, forecast, region_of_interest, weight, roi_metric
+        target, forecast, region_of_interest, roi_weight, roi_metric
     )
     constraint_penalty = calculate_constraint_penalty(
         target, forecast, constraints, tolerance_percentage
@@ -48,6 +63,29 @@ def calculate_combined_roi_metric(
     roi_metric,
     divide_by_target_range=True,
 ):
+    """
+    Calculate the combined metric for the region of interest and its complement.
+
+    Parameters:
+    -----------
+    target: np.ndarray
+        The target values. (n_timesteps,)
+    forecast: np.ndarray
+        The forecast values. (n_samples, n_timesteps)
+    region_of_interest: int, list of ints, slice, or boolean mask
+        The region of interest to apply the roi_metric to.
+    roi_weight: float
+        The weight to apply to the region of interest.
+    roi_metric: function
+        The metric to apply to the region of interest.
+    divide_by_target_range: bool, optional
+        Whether to divide the combined metric by the target range. Default is True.
+
+    Returns:
+    --------
+    combined_roi_metric: float
+        The combined metric value for the region of interest and its complement.
+    """
     roi_mask = format_roi_mask(region_of_interest, forecast.shape)
 
     weighted_roi_metric = calculate_masked_metric(
@@ -67,7 +105,26 @@ def calculate_combined_roi_metric(
 
 
 def calculate_masked_metric(target, forecast, roi_mask, roi_weight, roi_metric):
+    """
+    Calculate the weighted metric for a masked region of interest.
+    Parameters:
+    -----------
+    target: np.ndarray
+        The target values. (n_timesteps,)
+    forecast: np.ndarray
+        The forecast values. (n_samples, n_timesteps)
+    roi_mask: np.ndarray
+        The boolean mask for the region of interest.
+    roi_weight: float
+        The weight to apply to the region of interest.
+    roi_metric: function
+        The metric to apply to the region of interest.
+    Returns:
+    --------
+    weighted_roi_metric: float
+        The weighted metric value for the region of interest.
 
+    """
     roi_target = target[roi_mask]
     roi_forecast = forecast[:, roi_mask]
     roi_metric = roi_metric(target=roi_target, samples=roi_forecast)
@@ -78,21 +135,68 @@ def calculate_masked_metric(target, forecast, roi_mask, roi_weight, roi_metric):
 def calculate_constraint_penalty(
     target, forecast, constraints, tolerance_percentage, scale_factor=np.log(2)
 ):
-    penalty = 0
+    """
+    Calculate the penalty for violating constraints.
+
+    Parameters:
+    -----------
+    target: np.ndarray
+        The target values. (n_timesteps,)
+    forecast: np.ndarray
+        The forecast values. (n_samples, n_timesteps)
+    constraints: dict
+        A dictionary containing the constraints. The keys are "min" and "max".
+    tolerance_percentage: float
+        The percentage of the target range to use as a tolerance for the constraints.
+    scale_factor: float, optional
+        The scale factor to apply to the penalty. Default is np.log(2).
+
+    Returns:
+    --------
+    penalty: float
+        The penalty value for violating constraints.
+    """
+    penalties = []
     constraint_tolerance = (np.max(target) - np.min(target)) * tolerance_percentage
 
-    if "min" in constraints:
-        min_val = constraints["min"]
-        penalty += np.sum(np.maximum(0, (min_val - constraint_tolerance) - forecast))
+    for sample_forecast in forecast:
+        penalty = 0
+        if "min" in constraints:
+            min_val = constraints["min"]
+            penalty += np.sum(
+                np.maximum(0, (min_val - constraint_tolerance) - sample_forecast)
+            )
 
-    if "max" in constraints:
-        max_val = constraints["max"]
-        penalty += np.sum(np.maximum(0, forecast - (max_val + constraint_tolerance)))
+        if "max" in constraints:
+            max_val = constraints["max"]
+            penalty += np.sum(
+                np.maximum(0, sample_forecast - (max_val + constraint_tolerance))
+            )
 
-    return penalty * scale_factor / tolerance_percentage
+        penalties.append(penalty)
+
+    average_penalty = np.mean(penalties)
+    return average_penalty * scale_factor / tolerance_percentage
 
 
 def format_roi_mask(region_of_interest, forecast_shape):
+    """
+    Formats the region of interest mask based on the given inputs.
+
+    Parameters:
+    ----------
+    region_of_interest (int, list, slice, np.ndarray): The region of interest mask.
+        - If an int, a mask with a single True value at the specified index is returned.
+        - If a list of ints, a mask with True values at the specified indices is returned.
+        - If a slice, a mask with True values within the specified range is returned.
+        - If a boolean np.ndarray, the mask is returned as is.
+    forecast_shape (tuple): The shape of the forecast.
+
+    Returns:
+    -------
+        np.ndarray: The formatted region of interest mask.
+
+    """
     if isinstance(region_of_interest, int):
         mask = np.zeros(forecast_shape[1], dtype=bool)
         mask[region_of_interest] = True
@@ -160,6 +264,7 @@ if __name__ == "__main__":
 
     # Creating sample target and forecast ndarrays
     target_data = np.array([1, 2, 3, 4, 5])
+    # Original forecasts
     forecasts = [
         [1.0, 2.0, 3.0, 4.0, 5.0],  # Perfect forecast
         [1.1, 2.1, 3.1, 4.1, 5.1],  # Slightly off, respects constraints
@@ -169,6 +274,21 @@ if __name__ == "__main__":
         [1, 3, 4, 4, 5],  # Bad forecast in region of interest
         [2, 2, 3, 4, 4],  # Bad forecast in complement
     ]
+
+    # Number of samples per forecast
+    n_samples = 3
+
+    # Generate multiple samples per forecast
+    multi_sample_forecasts = np.array(
+        [
+            np.array(forecast)
+            + np.random.normal(0, 0.01, size=(n_samples, len(forecast)))
+            for forecast in forecasts
+        ]
+    )
+
+    # Transpose to (n_forecasts, n_samples, n_timesteps)
+    forecasts = np.transpose(multi_sample_forecasts, (0, 1, 2))
 
     # Initialize RegionOfInterestConstraintMetric with Min and Max Constraints
     constraints = {"min": 1, "max": 5}
@@ -192,12 +312,12 @@ if __name__ == "__main__":
 
     # Loop through each forecast and plot
     for i, forecast in enumerate(forecasts):
-        forecast = np.array(forecast).reshape(1, -1)
         metric_value = region_of_interest_constraint_metric(
             target=target_data,
             forecast=forecast,
             region_of_interest=region_of_interest,
-            weight=roi_weight,
+            roi_weight=roi_weight,
+            roi_metric=crps_by_quantile_mean,
             constraints=constraints,
             tolerance_percentage=tolerance_percentage,
         )
