94d6b2e602a7023f4a2ee605e43260fa8f570692
Valentina Zantedeschi <vzantedeschi@gmail.com>
2024-08-12 17:33:58 +0000

    black

--- benchmark/benchmark/tasks/causal_chambers.py
+++ benchmark/benchmark/tasks/causal_chambers.py
@@ -26,13 +26,13 @@ class WindTunnelTask(UnivariateCRPSTask):
         self.target_name = target_name
 
         super().__init__(seed=seed, fixed_config=fixed_config)
-    
+
     def _get_number_instances(self):
-        """ Returns number of different instances/windows this task comprises"""
+        """Returns number of different instances/windows this task comprises"""
         return len(self.possible_windows)
 
-    def _get_instance_by_idx(self, idx: int, downsample : str = None):
-        """ Returns instance corresponding to specified index, downsampled if required.
+    def _get_instance_by_idx(self, idx: int, downsample: str = None):
+        """Returns instance corresponding to specified index, downsampled if required.
 
         Args:
             idx (int): Instance index.
@@ -62,22 +62,22 @@ class WindTunnelTask(UnivariateCRPSTask):
             # downsample numerical variates, not averaging to avoid introducing new values
             past_time = past_time.resample(downsample).min()
             future_time = future_time.resample(downsample).min()
-        
+
         return window, past_time, future_time, text_covariates
 
-
     def random_instance(self, downsample: str = "1s"):
-        """ Sets random downsampled instance/window as task instance.
+        """Sets random downsampled instance/window as task instance.
 
         Args:
             downsample (str, optional): Downsampling rule (see https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.resample.html). Defaults to "1s".
         """
 
         window_idx = self.random.choice(self._get_number_instances())
-        self.window, self.past_time, self.future_time, self.covariates = self._get_instance_by_idx(window_idx, downsample)
-        
+        self.window, self.past_time, self.future_time, self.covariates = (
+            self._get_instance_by_idx(window_idx, downsample)
+        )
 
-    def verbalize_covariate(self, observations: pd.DataFrame, round_freq: str = 's'):
+    def verbalize_covariate(self, observations: pd.DataFrame, round_freq: str = "s"):
 
         covariate = observations[self.covariate_name]
         # intervention column = 0 when load is constant and = 1 when it changes wrt previous timestep
@@ -89,7 +89,11 @@ class WindTunnelTask(UnivariateCRPSTask):
             change_points.remove(0)
 
         # get datetimes from unix times, drop date as it is constant
-        timestamps = pd.to_datetime(observations.timestamp, unit="s").dt.round(freq=round_freq).dt.time
+        timestamps = (
+            pd.to_datetime(observations.timestamp, unit="s")
+            .dt.round(freq=round_freq)
+            .dt.time
+        )
 
         ans = f"The load is set to: {covariate.iloc[0]:.1f}"
         for c in change_points:
@@ -111,15 +115,15 @@ class SpeedFromLoad(WindTunnelTask):
 
         self.possible_windows = [
             Window(4, 0, 952, 1100),
-            Window(3, 300, 807, 1420),
             Window(7, 0, 613, 1000),
+            Window(3, 300, 807, 1420),
             Window(4, 0, 1886, 2000),
             Window(5, 0, 502, 600),
             Window(6, 0, 686, 880),
             Window(2, 0, 440, 700),
             Window(0, 0, 1159, 1300),
             Window(1, 0, 779, 900),
-            Window(1, 0, 779, 1400)
+            Window(1, 0, 779, 1400),
         ]
 
         self.background = "The wind tunnel is a chamber with one controllable fan that pushes air through it. We can control the load of the fan (corresponding to the duty cycle of the pulse-width-modulation signal) and measure its speed (in revolutions per minute). The fan is designed so its steady-state speed scales broadly linearly with the load. Unless completely powered off, the fan never operates below a certain speed, corresponding to a minimum effective load between 0.1 and 0.2."
--- benchmark/tests/test_causal_chambers.py
+++ benchmark/tests/test_causal_chambers.py
@@ -5,7 +5,7 @@ Unit tests for causal chambers tasks
 
 import pandas as pd
 
-from benchmark.tasks.causal_chambers import WindTunnelTask, SpeedFromLoad
+from benchmark.tasks.causal_chambers import SpeedFromLoad
 
 
 def test_deterministic_instance():
@@ -22,10 +22,10 @@ def test_deterministic_instance():
     assert len(past_time.columns) == 2
     assert len(future_time.columns) == 2
 
-    assert covariates.count('until') == 5
-    assert covariates.count('from') == 4
+    assert covariates.count("until") == 5
+    assert covariates.count("from") == 4
 
-    idx = 1
+    idx = 2
     window, past_time, future_time, covariates = task._get_instance_by_idx(idx)
 
     assert len(past_time.index) == window.future_start - window.history_start
@@ -34,8 +34,9 @@ def test_deterministic_instance():
     assert len(past_time.columns) == 2
     assert len(future_time.columns) == 2
 
-    assert covariates.count('until') == 6
-    assert covariates.count('from') == 5
+    assert covariates.count("until") == 6
+    assert covariates.count("from") == 5
+
 
 def test_downsampling():
 
@@ -44,5 +45,11 @@ def test_downsampling():
         task.random_instance()
 
         # downsampling to 1s, while original frequency should be ~7 Hertz
-        assert len(task.past_time.index) < (task.window.future_start - task.window.history_start) / 6
-        assert len(task.future_time.index) < (task.window.time_end - task.window.future_start) / 6
+        assert (
+            len(task.past_time.index)
+            < (task.window.future_start - task.window.history_start) / 6
+        )
+        assert (
+            len(task.future_time.index)
+            < (task.window.time_end - task.window.future_start) / 6
+        )
