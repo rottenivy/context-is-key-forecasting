82f25d25790276cd129cb01aa4e0b6ce1c0db664
Étienne Marcotte <etienne.marcotte@servicenow.com>
2024-08-16 19:58:35 +0000

    Switch to using the new implementation of CRPS

--- benchmark/benchmark/metrics/roi_metric.py
+++ benchmark/benchmark/metrics/roi_metric.py
@@ -1,14 +1,14 @@
 import numpy as np
-from benchmark.metrics.crps import crps_quantile
+from benchmark.metrics.crps import crps
 
 import matplotlib.pyplot as plt
 
 
-def crps_by_quantile_mean(target, samples):
-    if len(target) > 0:
-        return crps_quantile(target, samples)[0].mean() / len(target)
-    else:
-        return 0
+def mean_crps(target, samples):
+    """
+    The mean of the CRPS over all variables
+    """
+    return crps(target, samples).mean()
 
 
 def region_of_interest_constraint_metric(
@@ -16,7 +16,7 @@ def region_of_interest_constraint_metric(
     forecast,
     region_of_interest,
     roi_weight,
-    roi_metric=crps_by_quantile_mean,
+    roi_metric=mean_crps,
     constraints=None,
     tolerance_percentage=0.05,
 ):
@@ -34,7 +34,7 @@ def region_of_interest_constraint_metric(
     roi_weight: float
         The weight to apply to the region of interest.
     roi_metric: function, optional
-        The metric to apply to the region of interest. Default is crps_by_quantile_mean.
+        The metric to apply to the region of interest. Default is mean_crps.
     constraints: dict, optional
         A dictionary containing the constraints. The keys are "min" and "max". Default is None.
     tolerance_percentage: float, optional
@@ -235,144 +235,3 @@ def format_roi_mask(region_of_interest, forecast_shape):
         raise ValueError(
             "region_of_interest must be an int, a list of ints, a slice, or a boolean mask"
         )
-
-
-if __name__ == "__main__":
-
-    def plot_forecast(
-        ax, target, forecast, region_of_interest, metric_value, constraints, title
-    ):
-        ax.plot(target, label="Target", marker="o", color="g")
-        ax.plot(forecast.T, label="Forecast", linestyle="--", marker="x", alpha=0.7)
-
-        min_val = constraints.get("min", None)
-        max_val = constraints.get("max", None)
-        tolerance = tolerance_percentage * (np.max(target) - np.min(target))
-
-        if min_val is not None:
-            ax.axhline(y=min_val, color="r", linestyle="-", label="Min Constraint")
-            ax.axhline(
-                y=min_val - tolerance,
-                color="r",
-                linestyle=":",
-                label="Min Constraint - Tolerance",
-            )
-
-        if max_val is not None:
-            ax.axhline(y=max_val, color="b", linestyle="-", label="Max Constraint")
-            ax.axhline(
-                y=max_val + tolerance,
-                color="b",
-                linestyle=":",
-                label="Max Constraint + Tolerance",
-            )
-
-        ax.set_title(f"{title}\nMetric Value: {metric_value:.2f}")
-        ax.legend()
-        ax.set_xlabel("Timestep")
-        ax.set_ylabel("Value")
-        ax.set_ylim([0, 10])
-
-    # Creating sample target and forecast ndarrays
-    target_data = np.array([1, 2, 3, 4, 5])
-    # Original forecasts
-    forecasts = [
-        [1.0, 2.0, 3.0, 4.0, 5.0],  # Perfect forecast
-        [1.1, 2.1, 3.1, 4.1, 5.1],  # Slightly off, respects constraints
-        [5, 4, 3, 2, 1],  # Very off, respects constraints
-        [1, 2, 3, 4, 6],  # Slightly off, does not respect constraints
-        [6, 4, 2, 0, -2],  # Very off, does not respect constraints
-        [1, 3, 4, 4, 5],  # Bad forecast in region of interest
-        [2, 2, 3, 4, 4],  # Bad forecast in complement
-    ]
-
-    # Number of samples per forecast
-    n_samples = 3
-
-    # Generate multiple samples per forecast
-    multi_sample_forecasts = np.array(
-        [
-            np.array(forecast)
-            + np.random.normal(0, 0.01, size=(n_samples, len(forecast)))
-            for forecast in forecasts
-        ]
-    )
-
-    # Transpose to (n_forecasts, n_samples, n_timesteps)
-    forecasts = np.transpose(multi_sample_forecasts, (0, 1, 2))
-
-    # Initialize RegionOfInterestConstraintMetric with Min and Max Constraints
-    constraints = {"min": 1, "max": 5}
-    region_of_interest = slice(1, 4)
-    roi_weight = 0.91
-    tolerance_percentage = 0.1
-
-    # Titles for each forecast example
-    titles = [
-        "Perfect Forecast",
-        "Slightly Off, Respects Constraints",
-        "Very Off, Respects Constraints",
-        "Slightly Off, Does Not Respect Constraints",
-        "Very Off, Does Not Respect Constraints",
-        "Bad Forecast in Region of Interest",
-        "Bad Forecast in Complement",
-    ]
-
-    fig, axs = plt.subplots(4, 2, figsize=(15, 20))
-    fig.tight_layout(pad=5.0)
-
-    # Loop through each forecast and plot
-    for i, forecast in enumerate(forecasts):
-        metric_value = region_of_interest_constraint_metric(
-            target=target_data,
-            forecast=forecast,
-            region_of_interest=region_of_interest,
-            roi_weight=roi_weight,
-            roi_metric=crps_by_quantile_mean,
-            constraints=constraints,
-            tolerance_percentage=tolerance_percentage,
-        )
-        ax = axs[i // 2, i % 2]
-        plot_forecast(
-            ax,
-            target_data,
-            forecast,
-            region_of_interest,
-            metric_value,
-            constraints,
-            titles[i],
-        )
-        print(f"Forecast {i + 1} Metric Value: {metric_value:.2f}")
-
-    # Hide the last empty subplot if needed
-    if len(forecasts) % 2 != 0:
-        axs[-1, -1].axis("off")
-
-    plt.suptitle(
-        r"$\text{ROI\_crps} = \left( \sum_{i=1}^n w_i \cdot \text{CRPS}_i \right) \times \exp\left(penalty_{constraint} * \frac{\log(2)}{\text{tolerance\_percentage}}\right)$",
-        fontsize=14,
-        y=0.995,
-    )
-    plt.tight_layout()
-
-    txt = """
-            Each plot represents a single sample forecast.
-            Target in solid green, forecast in dashed yellow
-            The ground truth forecast is a line from 1 to 5.
-            The dashed line represents the forecast.
-            The red line represents the minimum constraint.
-            The blue line represents the maximum constraint.
-            The dotted lines represent the tolerance around the constraints 5(%).
-            The metric value is displayed in the title of each plot.
-            The metric value is calculated using the roi_metric * np.exp(penalty).
-            The roi_metric is calculated using CRPS (reduces to MAE for 1 sample).
-            The weight on the region of interest (timesteps 1,2,3) is 0.91.
-            The penalty is calculated using the constraints.
-            Hence, the metric values is roi_CRPS * exp(penalty).
-            """
-
-    plt.figtext(0.52, 0.1, txt, wrap=True, horizontalalignment="left", fontsize=12)
-
-    plt.savefig(
-        "/home/toolkit/starcaster/research-starcaster/benchmark/benchmark/metrics/forecast_comparison_functions.png"
-    )
