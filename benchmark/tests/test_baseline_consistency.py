bfee1557ea9a663ed0cac8aec49c6f13088310d0
Alexandre Drouin <alexandre.drouin1@gmail.com>
2024-08-20 14:38:08 +0000

    Add tests to check version are well defined

---
+++ benchmark/tests/test_baseline_consistency.py
@@ -0,0 +1,43 @@
+import pytest
+import types
+
+
+from benchmark.baselines.gpt_processes import GPTForecaster
+from benchmark.baselines.lag_llama import lag_llama
+from benchmark.baselines.llm_processes import LLMPForecaster
+from benchmark.baselines.naive import oracle_baseline, random_baseline
+from benchmark.baselines.statsmodels import (
+    ETSModelForecaster,
+    ExponentialSmoothingForecaster,
+)
+from benchmark.utils import get_all_parent_classes
+
+
+ALL_BASELINES = [
+    GPTForecaster,
+    lag_llama,
+    LLMPForecaster,
+    oracle_baseline,
+    random_baseline,
+    ETSModelForecaster,
+    ExponentialSmoothingForecaster,
+]
+
+
+@pytest.mark.parametrize("baseline", ALL_BASELINES)
+def test_version(baseline):
+    """
+    Test that the baseline defines a version attribute
+
+    """
+    assert (
+        "__version__" in baseline.__dict__
+    ), f"{baseline} should define a __version__ attribute"
+
+    # If the baseline is a class, check that all parent classes define a version attribute
+    if isinstance(baseline, type):
+        parents = get_all_parent_classes(baseline)
+        status = {t: "__version__" in t.__dict__ for t in parents}
+        assert all(
+            status.values()
+        ), f"All parents of {baseline} should define a __version__ attribute but found {status}"
