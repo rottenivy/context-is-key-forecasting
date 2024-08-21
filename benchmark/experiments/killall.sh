06ed49576883e245cf72f9cf195403436083a5d4
Alexandre Drouin <alexandre.drouin1@gmail.com>
2024-08-21 20:08:12 +0000

    Script to kill all exps

--- benchmark/experiments/killall.sh
+++ benchmark/experiments/killall.sh
@@ -1,1 +1,1 @@
-eai job ls --me --state alive --fields id,name | grep starcaster | cut -d ' ' -f1 | xargs eai job kill
+eai job ls --me --state alive --fields id,name | grep starcaster | cut -d ' ' -f1 | xargs eai job kill || echo "Nothing to kill."
