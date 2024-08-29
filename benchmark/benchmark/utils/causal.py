25c7b52e814d30a8eac9b86e336bb16d770605d2
jithendaraa <jithen.subra@gmail.com>
2024-08-29 19:36:46 +0000

    Index epsilon properly; Keep only 3 decimals for coeffs

--- benchmark/benchmark/utils/causal.py
+++ benchmark/benchmark/utils/causal.py
@@ -47,7 +47,7 @@ def parent_descriptions(W, L, node, desc):
             coeff_parent_vars = []
             for parent in parents:
                 coefficient = W[l, parent, node]
-                coeff_parent_vars.append(f"{coefficient} * X_{parent}")
+                coeff_parent_vars.append(f"{coefficient:.3f} * X_{parent}")
                 parent_vars.append(f"X_{parent}")
 
             expression = " + ".join(coeff_parent_vars)
@@ -57,7 +57,7 @@ def parent_descriptions(W, L, node, desc):
             for parent in parents:
                 coefficient = W[l, parent, node]
                 timestep_indexed_parent = f"X_{parent}" + "^{t-" + str(l) + "}"
-                coeff_parent_vars.append(f"{coefficient} * {timestep_indexed_parent}")
+                coeff_parent_vars.append(f"{coefficient:.3f} * {timestep_indexed_parent}")
 
         else:
             NotImplementedError(
@@ -66,10 +66,10 @@ def parent_descriptions(W, L, node, desc):
 
     if desc == "edge_weights_explicit_equation":
         if len(coeff_parent_vars) == 0:
-            res = f"X_{node}^" + "{t} = \epsilon."
+            res = f"X_{node}^" + "{t} = \epsilon_" + f"{node}^" + "{t}"
         else:
             expression = " + ".join(coeff_parent_vars)
-            res = f"X_{node}^" + "{t} = " + expression + " + \epsilon."
+            res = f"X_{node}^" + "{t} = " + expression + f" + \epsilon_{node}^" + "{t}"
 
     return res
 
