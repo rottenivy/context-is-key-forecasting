f5033185a1fbace4233a3f8e3b2c0f7c581fbe17
Étienne Marcotte <35576348+marcotet@users.noreply.github.com>
2024-09-10 16:03:20 -0400

    Switch the Half-day solar tasks to the NSRDB data (#542)
    * Switch the Half-day solar tasks to the NSRDB data
    
    * Update version numbers

--- benchmark/benchmark/tasks/solar_tasks.py
+++ benchmark/benchmark/tasks/solar_tasks.py
@@ -13,6 +13,155 @@ from gluonts.dataset.repository import get_dataset
 from ..base import UnivariateCRPSTask
 from ..config import DATA_STORAGE_PATH
 from ..metrics.constraints import MinConstraint
+from .nsrdb_tasks import download_all_nsrdb_datasets
+
+
+class BaseHalfDayNSRDBSolarForecastTask(UnivariateCRPSTask):
+    """
+    A task where the model is tasked to forecast the second half of the day,
+    for a dataset which has a strong daily periodicity.
+    The task comes with a textual description which can helps the model
+    learns the daily shape of the signal.
+    Use the NSRDB data instead of the solar_10min dataset.
+    Use the Direct Normal Irradiance, due to being more variable than the Global Horizontal Irradiance.
+    """
+
+    __version__ = "0.0.3"  # Modification will trigger re-caching
+
+    def random_instance(self):
+        # All lot of this work is repeated for all instances, so it wouldn't hurt to cache it.
+        all_data = download_all_nsrdb_datasets(interval=10)
+
+        # Select a random time series
+        ts_index = self.random.choice(len(all_data))
+        header = all_data[ts_index][0]
+        full_series = all_data[ts_index][1]["GHI"]
+
+        # The NSRDB dataset is for the whole year of 2022.
+        # Select a day for the forecast between 2022-07-01 (182nd day) and 2022-12-31 (365th day).
+        day = self.random.randint(low=181, high=365)
+        # Select the start of the forecast period, from 10:30 to 13:30.
+        forecast_time = self.random.randint(low=10 * 6 + 3, high=13 * 6 + 3 + 1)
+        # history = first ~12 hours of the day, target = second ~12 hours of the day
+        full_history_series = full_series.iloc[: (day * 24 * 6)]
+        history_series = full_series.iloc[
+            (day * 24 * 6) : (day * 24 * 6) + forecast_time
+        ]
+        future_series = full_series.iloc[
+            (day * 24 * 6) + forecast_time : (day * 24 * 6) + 24 * 6
+        ]
+
+        state = header["State"][2:-1]
+        country = header["Country"][2:-1]
+
+        background = self.get_background(
+            full_history_series,
+            state,
+            country,
+        )
+
+        # Instantiate the class variables
+        self.past_time = history_series.to_frame()
+        self.future_time = future_series.to_frame()
+        self.constraints = None
+        self.background = background
+        self.scenario = None
+
+        # No RoI need to be defined as the full prediction window is important
+
+    @abstractmethod
+    def get_background(
+        self,
+        full_history_series: pd.Series,
+        state: str,
+        country: str,
+    ) -> str:
+        """
+        Generate a textual hint for the model to know about the potential shape of the daily solar intensity.
+        """
+        pass
+
+    @property
+    def seasonal_period(self) -> int:
+        """
+        This returns the period which should be used by statistical models for this task.
+        If negative, this means that the data either has no period, or the history is shorter than the period.
+        """
+        # Not enough history for a single period
+        return -1
+
+
+class MinimalInfoHalfDaySolarForecastTask(BaseHalfDayNSRDBSolarForecastTask):
+    """
+    Version of the task where only the minimal background information is given.
+    """
+
+    _context_sources = ["c_i"]
+    _skills = BaseHalfDayNSRDBSolarForecastTask._skills + ["reasoning: deduction"]
+    __version__ = "0.0.2"  # Modification will trigger re-caching
+
+    def get_background(
+        self,
+        full_history_series: pd.Series,
+        state: str,
+        country: str,
+    ) -> str:
+        return "This series contains the amount of sunlight (in Watts per squared meter) arriving on a horizontal surface."
+
+
+class LocaleInfoHalfDaySolarForecastTask(BaseHalfDayNSRDBSolarForecastTask):
+    """
+    Version of the task where the state in which the data was collected is mentioned.
+    """
+
+    _context_sources = ["c_i"]
+    _skills = BaseHalfDayNSRDBSolarForecastTask._skills + ["reasoning: deduction"]
+    __version__ = "0.0.2"  # Modification will trigger re-caching
+
+    def get_background(
+        self,
+        full_history_series: pd.Series,
+        state: str,
+        country: str,
+    ) -> str:
+        return (
+            "This series contains the amount of sunlight (in Watts per squared meter) arriving on a horizontal surface, "
+            + f"for a location in {state}, {country}."
+        )
+
+
+class ZenithInfoHalfDaySolarForecastTask(BaseHalfDayNSRDBSolarForecastTask):
+    """
+    Version of the task where the average time at which the daily maximum is reached is mentioned.
+    """
+
+    _context_sources = ["c_i", "c_h"]
+    _skills = BaseHalfDayNSRDBSolarForecastTask._skills + ["reasoning: deduction"]
+    __version__ = "0.0.2"  # Modification will trigger re-caching
+
+    def get_background(
+        self,
+        full_history_series: pd.Series,
+        state: str,
+        country: str,
+    ) -> str:
+        def func(s):
+            t = s.idxmax()
+            return 3600 * t.hour + 60 * t.minute + t.second + 1e-6 * t.microsecond
+
+        # The resample is effectively a groupby based on the given frequency, so this gives us the mean
+        # time of the day at which the series is the highest.
+        past_90_days = full_history_series.iloc[-90 * 24 * 6 :]
+        mean_zenith_seconds = round(past_90_days.resample("D").apply(func).mean())
+        mean_zenith_formated = datetime.datetime.fromtimestamp(
+            mean_zenith_seconds
+        ).strftime("%H:%M:%S")
+
+        return (
+            "This series contains the amount of sunlight (in Watts per squared meter) arriving on a horizontal surface, "
+            + f"for a location in {state}, {country}.\n"
+            f"Over the previous 90 days, the maximum sunlight happened on average at {mean_zenith_formated}."
+        )
 
 
 class BaseHalfDaySolarForecastTask(UnivariateCRPSTask):
@@ -60,89 +209,6 @@ class BaseHalfDaySolarForecastTask(UnivariateCRPSTask):
         self.scenario = None
 
         # No RoI need to be defined as the full prediction window is important
-
-    @abstractmethod
-    def get_background(
-        self, full_history_series: pd.Series, forecast_date: pd.Timestamp
-    ) -> str:
-        """
-        Generate a textual hint for the model to know about the potential shape of the daily solar intensity.
-        """
-        pass
-
-    @property
-    def seasonal_period(self) -> int:
-        """
-        This returns the period which should be used by statistical models for this task.
-        If negative, this means that the data either has no period, or the history is shorter than the period.
-        """
-        # Not enough history for a single period
-        return -1
-
-
-class MinimalInfoHalfDaySolarForecastTask(BaseHalfDaySolarForecastTask):
-    """
-    Version of the task where only the minimal background information is given.
-    """
-
-    _context_sources = ["c_i"]
-    _skills = BaseHalfDaySolarForecastTask._skills + ["reasoning: deduction"]
-    __version__ = "0.0.1"  # Modification will trigger re-caching
-
-    def get_background(
-        self, full_history_series: pd.Series, forecast_date: pd.Timestamp
-    ) -> str:
-        return (
-            "This series contains the power production of a photovoltaic power plant."
-        )
-
-
-class LocaleInfoHalfDaySolarForecastTask(BaseHalfDaySolarForecastTask):
-    """
-    Version of the task where the state in which the data was collected is mentioned.
-    """
-
-    _context_sources = ["c_i"]
-    _skills = BaseHalfDaySolarForecastTask._skills + ["reasoning: deduction"]
-    __version__ = "0.0.1"  # Modification will trigger re-caching
-
-    def get_background(
-        self, full_history_series: pd.Series, forecast_date: pd.Timestamp
-    ) -> str:
-        return "This series contains the power production of a photovoltaic power plant in the state of Alabama."
-
-
-class ZenithInfoHalfDaySolarForecastTask(BaseHalfDaySolarForecastTask):
-    """
-    Version of the task where the average time at which the daily maximum is reached is mentioned.
-    """
-
-    _context_sources = ["c_i", "c_h"]
-    _skills = BaseHalfDaySolarForecastTask._skills + ["reasoning: deduction"]
-    __version__ = "0.0.1"  # Modification will trigger re-caching
-
-    def get_background(
-        self, full_history_series: pd.Series, forecast_date: pd.Timestamp
-    ) -> str:
-        def func(s):
-            p = s.idxmax()
-            t = p.start_time.time()
-            return 3600 * t.hour + 60 * t.minute + t.second + 1e-6 * t.microsecond
-
-        # The resample is effectively a groupby based on the given frequency, so this gives us the mean
-        # time of the day at which the series is the highest.
-        past_90_days = full_history_series.iloc[-90 * 24 * 6 :]
-        mean_zenith_seconds = round(past_90_days.resample("D").apply(func).mean())
-        mean_zenith_formated = (
-            datetime.datetime.fromtimestamp(mean_zenith_seconds)
-            .time()
-            .strftime("%H:%M:%S")
-        )
-
-        return (
-            "This series contains the power production of a photovoltaic power plant in the state of Alabama.\n"
-            f"Over the previous 90 days, the maximum power production happened on average at {mean_zenith_formated}."
-        )
 
 
 class SimilarLocationDaySolarForecastTask(BaseHalfDaySolarForecastTask):
