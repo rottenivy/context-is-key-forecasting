5c0cc3b5202df892a6ce9fab639fa5a204bfa352
AndrewRWilliams <andrew.williams@umontreal.ca>
2024-09-16 20:50:35 +0000

    add seed, model_size as arg

--- benchmark/benchmark/baselines/chronos.py
+++ benchmark/benchmark/baselines/chronos.py
@@ -6,14 +6,17 @@ from ..base import BaseTask
 from chronos import ChronosPipeline
 import torch
 
+from transformers import set_seed
+
 
 class ChronosForecaster(Baseline):
 
-    __version__ = "0.0.1"  # Modification will trigger re-caching
+    __version__ = "0.0.4"  # Modification will trigger re-caching
 
     def __init__(
         self,
         model_size,
+        seed=42
     ):
         """
         Get predictions from a Chronos model.
@@ -23,6 +26,7 @@ class ChronosForecaster(Baseline):
         This model requires a seasonal periodicity, which it currently gets from a
         hard coded association from the data index frequency (hourly -> 24 hours periods).
         """
+        self.seed=seed
         self.model_size = model_size
         super().__init__()
 
@@ -42,6 +46,7 @@ class ChronosForecaster(Baseline):
         This is primarily meant to be called inside a BaseTask constructor when doing rejection sampling or similar approaches.
         """
         # If there is no period, then disable the seasonal component of the model (seasonal_periods will be ignored)
+        set_seed(self.seed)
         device = "cuda" if torch.cuda.is_available() else "cpu"
         pipeline = ChronosPipeline.from_pretrained(
             f"amazon/chronos-t5-{self.model_size}",
@@ -49,7 +54,7 @@ class ChronosForecaster(Baseline):
             torch_dtype=torch.bfloat16,
         )
 
-        hist_values = torch.tensor(task_instance.past_time.values, dtype=torch.bfloat16)
+        hist_values = torch.tensor(task_instance.past_time.values, dtype=torch.bfloat16).flatten()
 
         # num_series, num_samples, num_timesteps
         model_preds = pipeline.predict(
@@ -59,7 +64,7 @@ class ChronosForecaster(Baseline):
         )
 
         # (1, num_samples, num_timesteps, num_series)
-        model_preds = model_preds.permute(1, 2, 0)[None, :]
+        model_preds = model_preds.permute(1, 2, 0)
 
         return model_preds.cpu().numpy()
 
@@ -67,21 +72,3 @@ class ChronosForecaster(Baseline):
     def cache_name(self) -> str:
         return f"{self.__class__.__name__}_{self.model_size}"
 
-
-# if __name__ == "__main__":
-#     # Dummy example to run the model
-#     class DummyTask:
-#         def __init__(self):
-#             self.past_time = pd.Series(
-#                 np.random.randn(100), index=pd.date_range("20210101", periods=100)
-#             )
-#             self.future_time = pd.Series(
-#                 np.random.randn(10), index=pd.date_range("20210501", periods=10)
-#             )
-
-#     task_instance = DummyTask()
-#     forecaster = ChronosForecaster(
-#         model_size="base",
-#     )
-#     predictions = forecaster(task_instance, n_samples=50)
-#     print(predictions)
