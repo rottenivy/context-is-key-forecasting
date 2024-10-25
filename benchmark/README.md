c9d97e8a44b4be680b07ae8df386d109613dc8d6
Étienne Marcotte <etienne.marcotte@servicenow.com>
2024-10-25 19:25:20 +0000

    Document the environment variables
    Both in the config.py and the README.md files.

--- benchmark/README.md
+++ benchmark/README.md
@@ -3,6 +3,52 @@
 <img width="946" alt="Push-button forecasting" src="https://github.com/ServiceNow/research-starcaster/assets/2374980/c3620848-7eda-46e3-bc11-6ed2d06022e4">
 
 
+## Setting environment variables
+
+Here is a list of all environment variables which the Context-is-Key benchmark will access:
+
+* Storage configuration
+  * `CIK_MODEL_STORE`\
+    Store the model weights of the baselines in this folder.\
+    Default to `"./models"`
+  * `CIK_DATA_STORE`\
+    Store the downloaded datasets in this folder.\
+    Default to `"benchmark/data"`
+  * `CIK_DOMINICK_STORE`\
+    Store the downloaded dataset for the tasks using the Dominick dataset in this folder.\
+    Default to `CIK_DATA_STORE` + `"/dominicks"`
+  * `CIK_TRAFFIC_DATA_STORE`\
+    Store the downloaded dataset for the tasks using the Traffic dataset in this folder.\
+    Default to `CIK_DATA_STORE` + `"/traffic_data"`
+  * `HF_HOME`\
+    Location of the cache when downloading some datasets from Hugging Face.\
+    Default to `CIK_DATA_STORE` + `"/hf_cache"`
+* Evaluation configuration
+  * `CIK_RESULT_CACHE`\
+    Store the output of the baselines in this folder, to avoid recomputing them.\
+    Default to `"./inference_cache"`
+  * `CIK_METRIC_SCALING_CACHE`\
+    Store the scaling factors for each task in this folder, to avoid recomputing them.\
+    Default to `"./metric_scaling_cache"`
+  * `CIK_METRIC_SCALING_CACHE`\
+    If set, the metric computation will also compute an estimate of the variance of the metric.\
+    By default, only compute the metric itself.
+* OpenAI configuration
+  * `CIK_OPENAI_USE_AZURE`\
+    If set to `"True"`, then the baselines using OpenAI models will use the Azure client, instead of the OpenAI client.\
+    Default to `"False"`
+  * `CIK_OPENAI_API_KEY`\
+    Must be set to use baselines using OpenAI models to your API key (either Azure or OpenAI depending on the value of `CIK_OPENAI_USE_AZURE`).
+  * `CIK_OPENAI_API_VERSION`\
+    If set, select the chosen API version when calling OpenAI model using the Azure client.
+  * `CIK_OPENAI_AZURE_ENDPOINT`\
+    Select the Azure endpoint to use when calling OpenAI models.
+* Nixtla configuration
+  * `CIK_NIXTLA_BASE_URL`\
+    Must be set to the Nixtla API URL to use the TimeGEN baseline.
+  * `NIXTLA_API_KEY`\
+    Must be set to your Nixtla API key to use the TimeGEN baseline.
+
 # Data Licenses 
 
 * Dominick's:
--- benchmark/benchmark/config.py
+++ benchmark/benchmark/config.py
@@ -7,12 +7,46 @@ import os
 
 from pathlib import Path
 
-# Model weight storage
+# Storage configuration
+# CIK_MODEL_STORE
+# Store the model weights of the baselines in this folder.
+# Default to "./models"
+# CIK_DATA_STORE
+# Store the downloaded datasets in this folder.
+# Default to "benchmark/data"
+# CIK_DOMINICK_STORE
+# Store the downloaded dataset for the tasks using the Dominick dataset in this folder.
+# Default to CIK_DATA_STORE + "/dominicks"
+# CIK_TRAFFIC_DATA_STORE
+# Store the downloaded dataset for the tasks using the Traffic dataset in this folder.
+# Default to CIK_DATA_STORE + "/traffic_data"
+# HF_HOME
+# Location of the cache when downloading some datasets from Hugging Face.
+# Default to CIK_DATA_STORE + "/hf_cache"
 MODEL_STORAGE_PATH = Path(os.environ.get("CIK_MODEL_STORE", "./models"))
 if not os.path.exists(MODEL_STORAGE_PATH):
     os.makedirs(MODEL_STORAGE_PATH, exist_ok=True)
+DATA_STORAGE_PATH = Path(os.environ.get("CIK_DATA_STORE", "benchmark/data"))
+if not DATA_STORAGE_PATH.exists():
+    DATA_STORAGE_PATH.mkdir(parents=True, exist_ok=True)
+DOMINICK_STORAGE_PATH = os.environ.get(
+    "CIK_DOMINICK_STORE", os.path.join(DATA_STORAGE_PATH, "dominicks")
+)
+TRAFFIC_STORAGE_PATH = os.environ.get(
+    "CIK_TRAFFIC_DATA_STORE", os.path.join(DATA_STORAGE_PATH, "traffic_data")
+)
+HF_CACHE_DIR = os.environ.get("HF_HOME", os.path.join(DATA_STORAGE_PATH, "hf_cache"))
 
 # Evaluation configuration
+# CIK_RESULT_CACHE
+# Store the output of the baselines in this folder, to avoid recomputing them.
+# Default to "./inference_cache"
+# CIK_METRIC_SCALING_CACHE
+# Store the scaling factors for each task in this folder, to avoid recomputing them.
+# Default to "./metric_scaling_cache"
+# CIK_METRIC_SCALING_CACHE
+# If set, the metric computation will also compute an estimate of the variance of the metric.
+# By default, only compute the metric itself.
 DEFAULT_N_SAMPLES = 50
 RESULT_CACHE_PATH = Path(os.environ.get("CIK_RESULT_CACHE", "./inference_cache"))
 METRIC_SCALING_CACHE_PATH = os.environ.get(
@@ -23,25 +57,24 @@ COMPUTE_METRIC_VARIANCE = bool(
 )  # bool("") == False
 
 # OpenAI configuration
+# CIK_OPENAI_USE_AZURE
+# If set to "True", then the baselines using OpenAI models will use the Azure client, instead of the OpenAI client.
+# Default to "False"
+# CIK_OPENAI_API_KEY
+# Must be set to use baselines using OpenAI models to your API key (either Azure or OpenAI depending on the value of CIK_OPENAI_USE_AZURE).
+# CIK_OPENAI_API_VERSION
+# If set, select the chosen API version when calling OpenAI model using the Azure client.
+# CIK_OPENAI_AZURE_ENDPOINT
+# Select the Azure endpoint to use when calling OpenAI models.
 OPENAI_USE_AZURE = os.environ.get("CIK_OPENAI_USE_AZURE", "False").lower() == "true"
 OPENAI_API_KEY = os.environ.get("CIK_OPENAI_API_KEY", "")
 OPENAI_API_VERSION = os.environ.get("CIK_OPENAI_API_VERSION", None)
 OPENAI_AZURE_ENDPOINT = os.environ.get("CIK_OPENAI_AZURE_ENDPOINT", None)
 
 # Nixtla configuration
+# CIK_NIXTLA_BASE_URL
+# Must be set to the Nixtla API URL to use the TimeGEN baseline.
+# NIXTLA_API_KEY
+# Must be set to your Nixtla API key to use the TimeGEN baseline.
 NIXTLA_BASE_URL = os.environ.get("CIK_NIXTLA_BASE_URL", None)
 NIXTLA_API_KEY = os.environ.get("CIK_NIXTLA_API_KEY", None)
-
-
-DATA_STORAGE_PATH = Path(os.environ.get("CIK_DATA_STORE", "benchmark/data"))
-if not DATA_STORAGE_PATH.exists():
-    DATA_STORAGE_PATH.mkdir(parents=True, exist_ok=True)
-
-# Data location for the data required for various tasks
-DOMINICK_STORAGE_PATH = os.environ.get(
-    "CIK_DOMINICK_STORE", os.path.join(DATA_STORAGE_PATH, "dominicks")
-)
-HF_CACHE_DIR = os.environ.get("HF_HOME", os.path.join(DATA_STORAGE_PATH, "hf_cache"))
-TRAFFIC_STORAGE_PATH = os.environ.get(
-    "CIK_TRAFFIC_DATA_STORE", os.path.join(DATA_STORAGE_PATH, "traffic_data")
-)
